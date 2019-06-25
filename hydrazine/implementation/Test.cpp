fcc73272aadf841cdbb7306965b2220fe2b4232d
kuroko <38317191+kuroko1t@users.noreply.github.com>
2019-06-25 21:42:06 +0900

    fix compile err (#1)

--- hydrazine/implementation/Test.cpp
+++ hydrazine/implementation/Test.cpp
@@ -23,8 +23,8 @@ namespace test
 	void Test::_seed()
 	{
 		if( seed == 0 )
-		{	
-			seed = (unsigned int) std::time( 0 );			
+		{
+			seed = (unsigned int) std::time( 0 );
 		}
 		random.seed( seed );
 	}
@@ -36,24 +36,24 @@ namespace test
 		verbose = false;
 		seed = 0;
 	}
-	
+
 	Test::~Test()
 	{
 
 	}
-	
+
 	void Test::test()
 	{
 		assert( !_testRun );
 		hydrazine::Timer Timer;
-		
+
 		_seed();
 		Timer.start();
 		_passed = doTest( );
 		Timer.stop();
 		_time = Timer.seconds();
 		_testRun = true;
-		
+
 		if( verbose )
 		{
 			#ifdef HAVE_MPICXX
@@ -68,70 +68,69 @@ namespace test
 			#endif
 		}
 	}
-	
+
 	std::string Test::toString() const
-	{	
-		std::stringstream stream;	
+	{
+		std::stringstream stream;
 		if( _testRun )
 		{
 			if( _passed )
 			{
-				stream << "Pass/Fail : Pass\n\n"; 
+				stream << "Pass/Fail : Pass\n\n";
 			}
 			else
 			{
 				stream << "Pass/Fail : Fail\n\n";
 			}
 		}
-		
+
 		stream << "\nName : " << name << "\n\n";
 		stream << testDescription() << "\n\n";
-		
+
 		if( _testRun )
 		{
 			stream << "Test Seed : " << seed << "\n";
 			stream << "Test time : " << _time << "\n\n";
 			stream << "Status : " << status.str() << "\n\n";
 		}
-							
+
 		return stream.str();
 	}
-	
+
 	std::string Test::testStatus() const
 	{
 		assert( _testRun );
 		return status.str();
 	}
-	
+
 	const std::string& Test::testName() const
 	{
 		return name;
 	}
-	
+
 	std::string Test::testDescription() const
 	{
-		return hydrazine::format( description, "Description: ", 
+		return hydrazine::format( description, "Description: ",
 			"             " );
 	}
-	
+
 	bool Test::passed() const
 	{
 		assert( _testRun );
 		return _passed;
 	}
-	
+
 	bool Test::run() const
 	{
 		return _testRun;
 	}
-	
+
 	hydrazine::Timer::Second Test::time() const
 	{
-		assert( _testRun );	
+		assert( _testRun );
 		return _time;
 	}
 
 }
 
 #endif
-
